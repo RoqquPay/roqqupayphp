819ad0b7f278940a8ce927bf1787bd71ae3b560a
Chibuokem ibezim <chibuokemibezim@gmail.com>
2021-01-27 15:56:24 +0100

    Library class and method created for end points available, with documentation at the moment

---
+++ src/RoqqupayphpClass.php
@@ -0,0 +1,324 @@
+<?php
+
+namespace Roqqupay\Roqqupayphp;
+
+class RoqqupayphpClass
+{
+    private $secKey;
+    private $base_url;
+
+//mine - RQ-SEC-zUsUq9EhPge2zOAMGdCX
+//demo - RQ-PUB-mBcZKVB8a1TO6GGa9M84
+    public function __construct($secKey = 'RQ-SEC-zUsUq9EhPge2zOAMGdCX')
+    {
+        $this->secKey = $secKey;
+        $this->base_url = 'https://api.roqqu.com/prod/v1';
+    }
+
+    /**
+    * Function to get token prices
+    * @return bool
+    */
+    public function getallTokenPrices()
+    {
+        $endpoint = '/prices';
+        $getPrices = $this->sendGetRequest($endpoint);
+
+        return $getPrices;
+    }
+
+    /**
+     * Function to return secret key
+     * @return mixed|string
+     */
+    public function getSecretKey()
+    {
+        return $this->secKey;
+    }
+
+    /**
+     * Function to get token price
+     * @param string $token
+     */
+    public function getTokenPrice($token = 'btc')
+    {
+        $endpoint = '/price?symbol='.$token;
+        $getPrice = $this->sendGetRequest($endpoint);
+
+        return $getPrice;
+    }
+
+    /**
+     * Function to get token price history
+     * @param string $symbol
+     * @return bool
+     */
+    public function getTokenPriceHistory($symbol = 'btc')
+    {
+        $endpoint = '/history?symbol='.$symbol;
+        $getPriceHistory = $this->sendGetRequest($endpoint);
+
+        return $getPriceHistory;
+    }
+
+    /**
+     * Function to get address data
+     * @param $symbol
+     * @param $address
+     * @return bool
+     */
+    public function getAddressData($symbol, $address)
+    {
+        $params = http_build_query(["symbol" => $symbol, "addr" => $address]);
+        $endpoint = "/blockchain/address?".$params;
+        $getAddressData = $this->sendGetRequest($endpoint);
+
+        return $getAddressData;
+    }
+
+    /**
+     * Function to generate wallet address
+     * @return mixed
+     */
+    public function generateWalletAddress($token = 'btc')
+    {
+        $endpoint = "/user/generate-wallet";
+        $params = 'token='.$token;
+        $generateWallet = $this->sendPostRequest($endpoint, $params);
+        return $generateWallet;
+    }
+
+    /**
+     * Function to get wallets
+     * @param string $token
+     * @return mixed
+     */
+    public function getWallets($token = 'btc')
+    {
+        $endpoint = '/user/wallets';
+        $params = 'token='.$token;
+        $wallets = $this->sendPostRequest($endpoint, $params);
+        return $wallets;
+    }
+
+    /**
+     * Function to delete wallet address
+     * @param $token
+     * @param $wallet
+     * @return mixed
+     */
+    public function deleteWallet($token, $wallet)
+    {
+        $endpoint = '/user/delete-wallet';
+        $params = 'token='.$token.'&address='.$wallet;
+        $deleteWallet = $this->sendPostRequest($endpoint, $params);
+        return $deleteWallet;
+    }
+
+    /**
+     * Function to send token to wallet address
+     * @param $token
+     * @param $amount
+     * @param $wallet
+     * @param string $memo
+     * @param string $tag
+     * @return mixed
+     */
+    public function sendToken($token, $amount, $wallet, $memo = '', $tag = '')
+    {
+        $endpoint = '/user/send-'.$token;
+        $params = 'amount='.$amount.'&wallet='.$wallet;
+        if($memo != ''){
+            $params.'&memo='.$memo;
+        }
+        if($tag != ""){
+            $params.'&tag='.$tag;
+        }
+        $sendToken = $this->sendPostRequest($endpoint, $params);
+        return $sendToken;
+    }
+
+    /**
+     * Function to verify bvn
+     * @param $bvn
+     * @return mixed
+     */
+    public function bvnVerify($bvn)
+    {
+        $endpoint = '/user/bvn-verify';
+        $params = 'bvn='.$bvn;
+        $verifyBvn = $this->sendPostRequest($endpoint, $params);
+        return $verifyBvn;
+    }
+
+    /**
+     * Function to get banks
+     * @return bool
+     */
+    public function getBanks()
+    {
+        $endpoint = '/banks';
+        $getBanks = $this->sendGetRequest($endpoint);
+        return $getBanks;
+    }
+
+    /**
+     * Function to resolve bank account
+     * @param $account_number
+     * @param $bank
+     * @return mixed
+     */
+    public function bankAccountResolve($account_number, $bank)
+    {
+        $endpoint = '/user/account-resolve';
+        $params = 'account_number='.$account_number.'&bank='.$bank;
+        $resolveBankAccount = $this->sendPostRequest($endpoint, $params);
+        return $resolveBankAccount;
+    }
+
+    /**
+     * Function to recharge airtime
+     * @param $phone_number
+     * @param $amount
+     * @return mixed
+     */
+    public function rechargeAirtime($phone_number, $amount)
+    {
+        $endpoint = '/user/recharge-airtime';
+        $params = 'phone_number='.$phone_number.'&amount='.$amount;
+        $rechargeAirtime = $this->sendPostRequest($endpoint, $params);
+        return $rechargeAirtime;
+    }
+
+    /**
+     * Function to get data bundles
+     * @param $provider
+     * @return bool
+     */
+    public function getDataBundles($provider)
+    {
+        $endpoint = '/data/subscriptions?provider='.$provider;
+        $dataBundles = $this->sendGetRequest($endpoint);
+        return $dataBundles;
+    }
+
+    /**
+     * Function to subscribe data
+     * @param $provider
+     * @param $variation_code
+     * @param $phone_number
+     * @return mixed
+     */
+   public function dataSubscription($provider, $variation_code, $phone_number)
+   {
+       $endpoint = '/user/pay-data';
+       $params = 'provider='.$provider.'&variation_code='.$variation_code.'&phone_number='.$phone_number;
+       $subscribe = $this->sendPostRequest($endpoint, $params);
+       return $subscribe;
+   }
+
+    /**
+     * Function to get cable bundles
+     * @param $provider
+     * @return bool
+     */
+   public function getCableBundles($provider)
+   {
+       $endpoint = '/cable/subscriptions?provider='.$provider;
+       $getBundles = $this->sendGetRequest($endpoint);
+       return $getBundles;
+   }
+
+    /**
+     * Function to verify meter number
+     * @param $provider
+     * @param $meter_type
+     * @param $meter_number
+     * @return bool
+     */
+   public function verifyMeterNumber($provider, $meter_type, $meter_number)
+   {
+       $endpoint = '/verify-meter?provider='.$provider.'&meter_type='.$meter_type.'&meter_number='.$meter_number;
+       $verifyMeterNumber = $this->sendGetRequest($endpoint);
+       return $verifyMeterNumber;
+   }
+
+    /**
+     * Function to recharge electric meter
+     * @param $provider
+     * @param $meter_type
+     * @param $meter_number
+     * @param $amount
+     * @param $phone_number
+     * @return mixed
+     */
+   public function electricMeterRecharge($provider, $meter_type, $meter_number, $amount, $phone_number)
+   {
+       $endpoint = '/user/pay-electric';
+       $params = 'provider='.$provider.'&meter_type='.$meter_type.'&meter_number='.$meter_number.'&amount='.$amount.'$phone_number='.$phone_number;
+       $recharge = $this->sendPostRequest($endpoint, $params);
+       return $recharge;
+   }
+
+    /**
+     * Function to send get request
+     * @param $url
+     * @return bool
+     */
+    private function sendGetRequest($endpoint)
+    {
+        $url = $this->base_url.$endpoint;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        //send request to server
+        $request = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($request, true);
+
+        if ($request) {
+            return $result;
+        } else {
+            var_dump($request);
+            die("Something went wrong while executing curl. Uncomment the var_dump line above this line to see what the issue is. Please check your CURL command to make sure everything is ok");
+//            return false;
+        }
+    }
+
+    /**
+     * Function to send post request
+     * @param $endpoint
+     * @param $params
+     * @return mixed
+     */
+    private function sendPostRequest($endpoint, $params)
+    {
+        $url = $this->base_url.$endpoint;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            [
+                'Authorization: Bearer '.$this->secKey
+            ]
+        );
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $request = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($request, true);
+
+        if ($request) {
+            return $result;
+        } else {
+            var_dump($request);
+            die("Something went wrong while executing curl. Uncomment the var_dump line above this line to see what the issue is. Please check your CURL command to make sure everything is ok");
+//            return false;
+        }
+    }
+}
---
+++ tests/RoqquTest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Roqqupay\Roqqupayphp\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Roqqupay\Roqqupayphp\RoqqupayphpClass;
+
+
+class RoqquTest extends TestCase
+{
+    /** @test */
+//    public function it_can_get_prices()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $prices = $roqquPayClass->getallTokenPrices();
+//        $this->assertEquals("success", $prices['status']);
+//        $this->assertEquals("prices retrieved", $prices['message']);
+//    }
+//
+//    /** @test */
+//    public function it_can_get_btc_price()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $price = $roqquPayClass->getTokenPrice('btc');
+//        $this->assertEquals("success", $price['status']);
+//        $this->assertEquals("price retrieved", $price['message']);
+//    }
+//
+//    /** @test */
+//    public function it_can_get_eth_price()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $price = $roqquPayClass->getTokenPrice('eth');
+//        $this->assertEquals("success", $price['status']);
+//        $this->assertEquals("price retrieved", $price['message']);
+//
+//    }
+//
+//    /** @test */
+//    public function it_can_get_eth_price_history()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $price = $roqquPayClass->getTokenPriceHistory('eth');
+//        $this->assertEquals("success", $price['status']);
+//        $this->assertEquals("price history retrieved successfully", $price['message']);
+//        $this->assertEquals("eth", $price['data'][0]['token']);
+//
+//    }
+//
+//    /** @test */
+//    public function it_can_get_btc_price_history()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $price = $roqquPayClass->getTokenPriceHistory('btc');
+//        $this->assertEquals("success", $price['status']);
+//        $this->assertEquals("price history retrieved successfully", $price['message']);
+//        $this->assertEquals("btc", $price['data'][0]['token']);
+//
+//    }
+//
+//    /** @test */
+//    public function it_can_get_address_data()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $addressData = $roqquPayClass->getAddressData("btc", "3EEhPSkNM86N755DgTdSgkJN4AFaZCC5jU");
+//        $this->assertEquals("success", $addressData['status']);
+//        $this->assertEquals("blockchain data retrieved successfully", $addressData['message']);
+//        $this->assertEquals("bitcoin", $addressData["data"]["network"]);
+//    }
+//
+//    /** @test */
+//    public function it_can_generate_wallet_address()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $generateWallet = $roqquPayClass->generateWalletAddress("btc");
+//        $this->assertEquals("success", $generateWallet['status']);
+//        $this->assertEquals("wallet created successfully", $generateWallet['message']);
+//        $this->assertEquals("btc", $generateWallet['data']['type']);
+//    }
+
+//    /** @test */
+//    public function it_can_get_wallets()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $wallets = $roqquPayClass->getWallets('btc');
+//        $this->assertEquals("success", $wallets['status']);
+//        $this->assertEquals("wallets retrieved successfully", $wallets['message']);
+//
+//    }
+
+//    /** @test */
+//    public function it_can_delete_wallet()
+//    {
+//
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $deleteWallet = $roqquPayClass->deleteWallet('btc', '34MRPoy9A88mXzAhMMGQYCiADm2twJhArv');
+//        $this->assertEquals("success", $deleteWallet['status']);
+//        $this->assertEquals("wallet deleted successfully", $deleteWallet['message']);
+//    }
+
+//    /** @test */
+//    public function it_can_get_banks()
+//    {
+//        $roqquPayClass = new RoqqupayphpClass();
+//        $getBanks = $roqquPayClass->getBanks();
+//        $this->assertEquals("success", $getBanks['status']);
+//       $this->assertEquals("all banks retrieved", $getBanks['message']);
+//    }
+
+//        /** @test */
+//      public function it_can_resolve_bank_account()
+//      {
+//          $roqquPayClass = new RoqqupayphpClass();
+//          $resolveAccount = $roqquPayClass->bankAccountResolve('3051559336', 'First Bank of Nigeria Limited');
+//          $this->assertEquals("success", $resolveAccount['status']);
+//          $this->assertEquals("account details retrieved successfully", $resolveAccount['message']);
+//          $this->assertEquals("ONOMOR ESEOGHENE BENJAMIN", $resolveAccount['data']);
+//      }
+
+//       /** @test */
+//      public function it_can_get_data_bundles()
+//      {
+//          $roqquPayClass = new RoqqupayphpClass();
+//          $dataBundles = $roqquPayClass->getDataBundles('mtn');
+//          $this->assertEquals("success", $dataBundles['status']);
+//          $this->assertEquals("available subscriptions retrieved", $dataBundles['message']);
+//      }
+
+//       /** @test */
+//      public function it_can_get_cable_bundles()
+//      {
+//          $roqquPayClass = new RoqqupayphpClass();
+//          $cableBundles = $roqquPayClass->getCableBundles('dstv');
+//          $this->assertEquals("success", $cableBundles['status']);
+//         $this->assertEquals("available subscriptions retrieved", $cableBundles['message']);
+//
+//      }
+
+      /** @test */
+     public function it_can_verify_meter_number()
+     {
+         $roqquPayClass = new RoqqupayphpClass();
+         $verifyMeterNumber = $roqquPayClass->verifyMeterNumber('ikeja', 'prepaid', '45055844760');
+         $this->assertEquals("success", $verifyMeterNumber['status']);
+         $this->assertEquals("user info retrieved successfully", $verifyMeterNumber['message']);
+     }
+
+}
